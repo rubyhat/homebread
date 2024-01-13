c0624b399d0dfb430255ea8e5937fdf35e8dd914
Vladislav <dreamervt8@gmail.com>
2024-01-13 20:38:28 +0600

    add images

--- src/modules/HomeModule/components/CardList/cardsData.ts
+++ src/modules/HomeModule/components/CardList/cardsData.ts
@@ -1,8 +1,77 @@
 export const cards = [
   {
-    img: "",
+    img: "/static/images/img-6.webp",
     textItems: [],
-    priceButton: {},
-    targetButton: {},
+    priceButton: {
+      text: "40 000 тг",
+      discount: "",
+      color: "#8d5b28",
+      href: "https://t.me/Tanyusha_ya",
+    },
+    targetButton: {
+      text: "Записаться",
+      color: "#228b22",
+      href: "https://t.me/Tanyusha_ya",
+    },
+  },
+  {
+    img: "/static/images/img-9.webp",
+    textItems: [],
+    priceButton: {
+      text: "20 000 тг",
+      discount: "",
+      color: "#8d5b28",
+      href: "https://t.me/Tanyusha_ya",
+    },
+    targetButton: {
+      text: "Записаться",
+      color: "#228b22",
+      href: "https://t.me/Tanyusha_ya",
+    },
+  },
+  {
+    img: "/static/images/img-4.webp",
+    textItems: [],
+    priceButton: {
+      text: "40 000 тг",
+      discount: "",
+      color: "#8d5b28",
+      href: "https://t.me/Tanyusha_ya",
+    },
+    targetButton: {
+      text: "Записаться",
+      color: "#228b22",
+      href: "https://t.me/Tanyusha_ya",
+    },
+  },
+  {
+    img: "/static/images/img-8.webp",
+    textItems: [],
+    priceButton: {
+      text: "3 000 тг",
+      discount: "",
+      color: "#8d5b28",
+      href: "https://t.me/Tanyusha_ya",
+    },
+    targetButton: {
+      text: "Записаться",
+      color: "#228b22",
+      href: "https://t.me/Tanyusha_ya",
+    },
+  },
+  {
+    img: "/static/images/img-5.webp",
+    textItems: [],
+    priceButton: {
+      text: "19 900 тг",
+      discount: "",
+      color: "#8d5b28",
+      href: "https://t.me/Tanyusha_ya",
+    },
+    targetButton: {
+      text: "Записаться",
+      color: "#228b22",
+      href: "https://t.me/Tanyusha_ya",
+    },
   },
 ];
