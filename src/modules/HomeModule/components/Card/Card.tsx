ccd206959f65540752ce19cc9d687fb17c15f9a9
Vladislav <dreamervt8@gmail.com>
2024-01-13 23:16:56 +0600

    add card list

--- src/modules/HomeModule/components/Card/Card.tsx
+++ src/modules/HomeModule/components/Card/Card.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { CardItem } from "../CardList/cardsData";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 interface CardProps {
   card: CardItem;
 }
 
 export const Card = ({ card }: CardProps) => {
-  return <Box>{card.title}</Box>;
+  return (
+    <Box
+      sx={{
+        padding: 1,
+        borderRadius: 2,
+        border: "2px solid",
+        borderColor: "var(--accent-color)",
+      }}
+    >
+      <Typography>{card.title}</Typography>
+      {card.textItems.length > 0 && (
+        <Box component="ul">
+          {card.textItems.map((textItem, index) => (
+            <Box
+              key={index}
+              component="li"
+              dangerouslySetInnerHTML={{ __html: textItem }}
+            />
+          ))}
+        </Box>
+      )}
+    </Box>
+  );
 };
--- src/modules/HomeModule/components/CardList/CardList.tsx
+++ src/modules/HomeModule/components/CardList/CardList.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { CardItem, cardsData } from "./cardsData";
 import { Card } from "../Card";
+import { Grid } from "@mui/material";
 
 export const CardList = () => {
   return (
     <>
-      {cardsData.map((card: CardItem) => (
-        <Card card={card} />
+      {cardsData.map((card: CardItem, index) => (
+        <Grid item xs={12} key={index}>
+          <Card card={card} />
+        </Grid>
       ))}
     </>
   );
