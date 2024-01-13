6875cb08b65d2ea4022eed91881648d05739c80b
Vladislav <dreamervt8@gmail.com>
2024-01-13 23:17:16 +0600

    add text for first card, add buttons in card

--- src/modules/HomeModule/components/Card/Card.tsx
+++ src/modules/HomeModule/components/Card/Card.tsx
@@ -10,22 +10,76 @@ export const Card = ({ card }: CardProps) => {
   return (
     <Box
       sx={{
-        padding: 1,
+        padding: 2,
         borderRadius: 2,
         border: "2px solid",
         borderColor: "var(--accent-color)",
       }}
     >
-      <Typography>{card.title}</Typography>
+      <Typography
+        component="h6"
+        variant="h6"
+        sx={{ color: "var(--accent-color)", fontWeight: 700 }}
+      >
+        {card.title}
+      </Typography>
       {card.textItems.length > 0 && (
         <Box component="ul">
           {card.textItems.map((textItem, index) => (
             <Box
               key={index}
               component="li"
-              dangerouslySetInnerHTML={{ __html: textItem }}
-            />
+              sx={{
+                marginTop: 0.75,
+              }}
+            >
+              <Typography
+                component="p"
+                variant="body1"
+                dangerouslySetInnerHTML={{ __html: textItem }}
+              />
+            </Box>
           ))}
+          <Box>
+            <Box
+              component="a"
+              href={card.priceButton.href}
+              target="_blank"
+              sx={{
+                backgroundColor: card.priceButton.color,
+                padding: 2,
+                width: 1,
+                marginTop: 2,
+                color: "#fff",
+                borderRadius: 2,
+                display: "block",
+                textAlign: "center",
+                fontWeight: 500,
+                fontSize: "18px",
+              }}
+            >
+              {card.priceButton.text}
+            </Box>
+            <Box
+              component="a"
+              href={card.targetButton.href}
+              target="_blank"
+              sx={{
+                backgroundColor: card.targetButton.color,
+                padding: 2,
+                width: 1,
+                marginTop: 2,
+                color: "#fff",
+                borderRadius: 2,
+                display: "block",
+                textAlign: "center",
+                fontWeight: 500,
+                fontSize: "18px",
+              }}
+            >
+              {card.targetButton.text}
+            </Box>
+          </Box>
         </Box>
       )}
     </Box>
