d876a0d102c8ed5c0599375454bbead33a08a4cd
Vladislav <dreamervt8@gmail.com>
2024-01-14 17:26:12 +0600

    upd footer

--- src/modules/AppModule/components/Footer/Footer.tsx
+++ src/modules/AppModule/components/Footer/Footer.tsx
@@ -1,3 +1,23 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
+
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              padding: "16px 0",
+              borderTop: "2px solid",
+              borderColor: "var(--accent-color)",
+            }}
+          >
+            <Typography variant="body1" fontWeight={500}>
+              Обучение домашней выпечке. 2020-2024.
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
